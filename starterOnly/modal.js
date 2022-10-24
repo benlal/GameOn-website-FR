fa9632aff986533e3db8886dbc3e61b907481bbf
Benjamin Lallemand <contact.benjaminlallemand@gmail.com>
2022-10-24 13:51:58 +0200

    Avancement sur #3 et #4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,10 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
+// const btnSubmit = document.getElementsByClassName('btn-submit')[0];
+const confirmationMessage = document.querySelector(".confirmation-message");
+const confirmationBtn = document.getElementById('close-btn');
+const form = document.getElementsByTagName('form')[0];
 
 // launch modal event
 modalBtn.forEach((btn) =>
@@ -31,11 +35,15 @@ function closeModal(){
   modalbg.style.display = "none";
 }
 
-let btnSubmit = document.getElementsByClassName('btn-submit')[0];
+//prevent default behavior of form
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+// })
 
-btnSubmit.addEventListener("click", inputCheck);
+// btnSubmit.addEventListener("click", inputCheck);
 
-function inputCheck () {
+//define validate / inputCheck
+function validate () {
 
   const firstName = document.getElementById('first');
   const lastName = document.getElementById('last');
@@ -49,25 +57,19 @@ function inputCheck () {
   const location5 = document.getElementById('location5');
   const location6 = document.getElementById('location6');
   const conditionsCheckbox = document.getElementById('checkbox1');
-  const locationIcon = document.getElementsByClassName('checkbox-icon')[0];
+  const locationIcon = document.getElementsByClassName('checkbox-icon');
   const conditionsIcon = document.getElementsByClassName('checkbox-icon')[6];
-
-
-
-
 
   // firstname validation
   let firstValidation;
   if (firstName.value.length < 2 || firstName.value == null) {
     firstValidation = false;
-    firstName.style.border = 'solid red 2px';
+    firstName.classList.add("empty");
     first_error.style.display = 'flex';
     first_error.textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
-    first_error.style.fontSize = '12px';
-    first_error.style.color = 'red';
   } else {
     firstValidation = true;
-    firstName.style = 'default';
+    firstName.classList.remove('empty');
     first_error.style.display = 'none';
   }
 
@@ -75,14 +77,12 @@ function inputCheck () {
   let lastValidation;
   if (lastName.value.length < 2 || lastName.value == null) {
     lastValidation = false;
-    lastName.style.border = 'solid red 2px';
+    lastName.classList.add("empty");
     last_error.style.display = 'flex';
     last_error.textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
-    last_error.style.fontSize = '12px';
-    last_error.style.color = 'red';
   } else {
     lastValidation = true;
-    lastName.style = 'default';
+    lastName.classList.remove('empty');
     last_error.style.display = 'none';
   }
 
@@ -90,45 +90,39 @@ function inputCheck () {
   let emailValidation;
   if (email.value.match (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
     emailValidation = true;
-    email.style = 'default';
+    email.classList.remove('empty');
     email_error.style.display = 'none';
   } else {
     emailValidation = false;
-    email.style.border = 'solid red 2px';
+    email.classList.add("empty");
     email_error.style.display = 'flex';
     email_error.textContent = 'Veuillez entrer une adresse valide.';
-    email_error.style.fontSize = '12px';
-    email_error.style.color = 'red';
   }
 
   // birthdate validation
   let birthdateValidation;
   if (birthdate.value) {
     birthdateValidation = true;
-    birthdate.style = 'default';
+    birthdate.classList.remove('empty');
     birthdate_error.style.display = 'none';
   } else {
     birthdateValidation = false;
-    birthdate.style.border = 'solid red 2px';
+    birthdate.classList.add("empty");
     birthdate_error.style.display = 'flex';
     birthdate_error.textContent = 'Vous devez entrer votre date de naissance.';
-    birthdate_error.style.fontSize = '12px';
-    birthdate_error.style.color = 'red';
   }
 
   // quantity validation
   let quantityValidation;
   if (quantity.value.match (/^\d+$/)) {
     quantityValidation = true;
-    quantity.style = 'default';
+    quantity.classList.remove('empty');
     quantity_error.style.display = 'none';
   } else {
     quantityValidation = false;
-    quantity.style.border = 'solid red 2px';
+    quantity.classList.add("empty");
     quantity_error.style.display = 'flex';
     quantity_error.textContent = 'Veuillez entrer votre nombre de participations.';
-    quantity_error.style.fontSize = '12px';
-    quantity_error.style.color = 'red';
   }
 
   // location validation
@@ -137,13 +131,20 @@ function inputCheck () {
     locationValidation = true;
     location_error.style.display = 'none';
     locationIcon.style = 'default';
+    let i = 0;
+    while (i < 6) {
+      locationIcon[i].style.border = 'solid #279e7a 2px';
+      i++
+    }
   } else {
     locationValidation = false;
     location_error.style.display = 'flex';
     location_error.textContent = 'Vous devez choisir une option.';
-    location_error.style.fontSize = '12px';
-    location_error.style.color = 'red';
-    locationIcon.style.border = 'solid red 2px';
+    let i = 0;
+    while (i < 6) {
+      locationIcon[i].style.border = 'solid red 2px';
+      i++
+    }
   }
 
   // checkbox validation
@@ -156,14 +157,13 @@ function inputCheck () {
     checkboxValidation = false;
     checkbox_error.style.display = 'flex';
     checkbox_error.textContent = 'Vous devez vérifier que vous acceptez les termes et conditions.';
-    checkbox_error.style.fontSize = '12px';
-    checkbox_error.style.color = 'red';
     conditionsIcon.style.border = 'solid red 2px';
   }
 
   if (firstValidation == true && lastValidation == true && emailValidation == true && quantityValidation == true && locationValidation == true && checkboxValidation == true) {
-    prompt('Votre inscription a été prise en compte.');
-  } else {
-    event.preventDefault();
-  }
+    closeBtn.style.display = 'none';
+    form.style.display = 'none';
+    confirmationMessage.style.display = 'block';
+    confirmationBtn.addEventListener("click", closeModal);
+  }  
 }
